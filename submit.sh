576c84ddddf53f890bfc50760bd0581752a34e30
oooozhizhi <651482179@qq.com>
2017-12-22 18:18:46 +0800

    blog update

--- submit.sh
+++ submit.sh
@@ -3,6 +3,7 @@ ls -l
 hexo generate
 cp -R public/* ../../yizhi/.git/oooozhizhi.github.io
 cd ../../yizhi/.git/oooozhizhi.github.io
+git pull --rebase origin master
 git add .
 git commit -m "blog update"
 git push origin master
